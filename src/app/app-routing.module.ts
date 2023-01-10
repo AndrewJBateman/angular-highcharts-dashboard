71c3341f5efe640db7e9b296a8cc6746e4faf250
Andrew Bateman <gomezbateman@yahoo.com>
2023-01-10 12:28:12 +0100

    chore: ng material update to v15, reduce app files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { PostsComponent } from './modules/posts/posts.component';
 import { ArticlesComponent } from './modules/articles/articles.component';
 import { ContactsComponent } from './modules/contacts/contacts.component';
 import { LeadsComponent } from './modules/leads/leads.component';
-
 
 const routes: Routes = [{
   path: '',
